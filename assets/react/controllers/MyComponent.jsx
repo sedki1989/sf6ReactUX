72603dbdd05babf7217de1bd771542c83bc73e3f
sedki <mgdsadok@gmail.com>
2023-02-23 14:10:39 +0100

    init commit

---
+++ assets/react/controllers/MyComponent.jsx
@@ -0,0 +1,14 @@
+// assets/react/controllers/MyComponent.jsx
+import React from 'react';
+import { useState } from "react";
+import "../../styles/app.css";
+import Counter from './Counter.jsx';
+import MyModal from './MyModal.jsx'
+
+export default function () {
+  return (
+    <>
+        <MyModal />
+    </>
+  );
+}
